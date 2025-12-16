9c66361217c992706e0779c8f9c95972dcfe3c4c
victor depina <victor.depina@gong.io>
2025-12-16 16:44:15 -0500

    Init commit

---
+++ dot_omz/history.zsh
@@ -0,0 +1,13 @@
+# HISTFILE=$HOME/.zsh_history
+# HISTSIZE=100000
+# SAVEHIST=$HISTSIZE
+
+# setopt hist_ignore_all_dups # remove older duplicate entries from history
+# setopt hist_reduce_blanks # remove superfluous blanks from history items
+# setopt inc_append_history # save history entries as soon as they are entered
+# setopt share_history # share history between different instances of the shell
+
+# bindkey '^[[A' history-substring-search-up
+# bindkey '^[[B' history-substring-search-down
+# bindkey -M vicmd 'k' history-substring-search-up
+# bindkey -M vicmd 'j' history-substring-search-down
